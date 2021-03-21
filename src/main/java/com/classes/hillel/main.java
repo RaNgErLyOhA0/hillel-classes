41b96a9b5f4ff2ed8e8446f4b1380b5b7016dce5
savchuk <darkgoanna@gmail.com>
2021-03-21 13:38:06 +0200

    initial commit

---
+++ src/main/java/com/classes/hillel/main.java
@@ -0,0 +1,12 @@
+package main.java.com.classes.hillel;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("hello from git!");
+        System.out.println("how are you");
+
+
+    }
+
+
+}
