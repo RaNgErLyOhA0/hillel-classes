727d3c526dafcd653740401f08d6dbe6a57c5d3a
savchuk <darkgoanna@gmail.com>
2021-03-21 14:57:02 +0200

    Created arrays; wrote array to the console

--- src/main/java/com/classes/hillel/main.java
+++ src/main/java/com/classes/hillel/main.java
@@ -2,11 +2,20 @@ package main.java.com.classes.hillel;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("hello from git!");
-        System.out.println("how are you");
+        String[] array = new String[2];
+        int[] array2 = {1, 2, 3};
 
+
+        array[0] = "GG";
+        array[1] = "SO EZ";
+        System.out.println(array[array.length-1]);
+
+       // for (int i = 0; i < array.length; i++) {
+      //      System.out.println(array[i]);
+    //    }
+    //    for (int i = 0; i <array2.length; i++) {
+    //        System.out.println(array2[i]);
+    //    }
 
     }
-
-
 }
